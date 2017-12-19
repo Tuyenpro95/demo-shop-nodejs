5d1c68499817791cf1b20e1b69faacf16214a56a
ngovanhuong <ngovanhuong21994@gmail.com>
2017-12-20 01:33:49 +0300

    added session and pasport

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var passport = require('passport');
 
 
 
@@ -10,6 +12,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(session({
+	secret: process.env.SESSION_SECRET
+}))
+app.use(passport.initialize())
+app.use(passport.session)
+
 
 app.get('/', (req,res) => {
 	res.render('index')
